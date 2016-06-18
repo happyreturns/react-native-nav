e6a73773b85643658ad5bd8a0455683346d1d858
Timothy Brown <tim@happyreturns.com>
2016-06-17 18:40:09 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,67 @@
 # react-native-nav
-A "Batteries Included" implementation of react-native's navigator
+A "Batteries Included" wrapper for react-native's navigator
+
+
+## Props
+
+* expandRoute `function(route)` This optional function allows for the expansion of a simple route (like {id: "routeName"}) into something renderable (like {component: MyComponent, title: "Foo"}).  Mostly used to keep the various `push` calls from having to specify everything. Due to the internals of react-native, this function should (weirdly) modify the route object in place rather than return a new one.
+
+Any additional prop passed to react-native-nav will be passed to the navigator, so those remain as normal.
+
+## Route attributes
+
+All attributes are optional except for `component`.
+
+| Name | Type | Description |
+| --- | --- | --- |
+| component | React Native class | The class to render, not an element.  Should be something like `MyComponent`, NOT `<MyComponent />`. |
+| passProps | Object | Any extra props that should be added to the component. |
+| title | string | The title of the scene.  Will appear in the center of the nav bar, and then used as the previous button unless overridden. |
+| appearFrom | string | Options are `bottom` or `left`.  If empty, the new scene will appear from the right. |
+| leftElement | React Native element | The element to use for the left nav button. In most cases, this should be an instance of LeftButton. |
+| leftButtonText | string | The text to display for the left nav button. Mutually exclusive with `leftButtonIcon`. |
+| leftButtonIcon | string | The icon name from the ionicons of `react-native-vector-icons` to be used as the left nav button. Mutually exclusive with `leftButtonText`. |
+| onPressLeftButton | function | Called when the left nav button is pressed. |
+| rightElement | React Native element | The element to use for the right nav button. In most cases, this should be an instance of RightButton. |
+| rightButtonText | string | The text to display for the right nav button. Mutually exclusive with `rightButtonIcon`. |
+| rightButtonIcon | string | The icon name from the ionicons of `react-native-vector-icons` to be used as the right nav button. Mutually exclusive with `rightButtonText`. |
+| onPressRightButton | function | Called when the right nav button is pressed. |
+
+## Usage
+
+```javascript
+
+import React, { Component } from 'react'
+import Navigation from 'react-native-nav'
+
+class Welcome extends Component {
+  render() { ... }
+}
+
+class Example extends Component {
+  expandRoute(route) {
+    if (route.id === 'welcome') {
+      route.component = Welcome
+    }
+  }
+
+  render() {
+    return (
+      <Navigation ref="nav"
+        initialRoute={{
+          id: 'Welcome',
+        }}
+        // Used to keep having to repeat common route information
+        expandRoute={this.expandRoute}
+        onWillFocus={function(route) {
+          // Extra props will be passed to the navigator
+          console.log(route)
+        }}
+       ></Navigation>
+    )
+  }
+}
+
+```
+
+
---
+++ navigation-ios.js
@@ -0,0 +1,204 @@
+// -*- mode: jsx -*-
+'use strict'
+
+import React, { Component } from 'react'
+import {
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  Navigator,
+  InteractionManager,
+} from 'react-native'
+import Icon from 'react-native-vector-icons/Ionicons'
+
+class LeftButton extends Component {
+  render() {
+    return (
+      <TouchableOpacity
+        onPress={this.props.onPress}
+        style={styles.navBarLeftButton}
+      >
+        <Icon name="ios-arrow-back" style={[styles.navBarButtonText, styles.navBarBackIcon]} />
+        <Text style={[styles.navBarText, styles.navBarButtonText]}>
+          {this.props.children}
+        </Text>
+      </TouchableOpacity>
+    )
+  }
+}
+
+class RightButton extends Component {
+  render() {
+    return (
+      <TouchableOpacity
+        onPress={this.props.onPress}
+        disabled={this.props.disabled}
+        style={styles.navBarRightButton}
+      >
+        <Text style={[styles.navBarText, styles.navBarButtonText, (this.props.disabled ? styles.disabled : null)]}>
+          {this.props.children}
+        </Text>
+      </TouchableOpacity>
+    )
+  }
+}
+
+const NavigationBarRouteMapper = {
+  LeftButton: function(route, navigator, index, navState) {
+    if (typeof route.leftElement !== 'undefined') {
+      return route.leftElement
+    }
+
+    // Manually set
+    if (route.leftButtonText || route.leftButtonIcon) {
+      return (
+        <LeftButton onPress={route.onPressLeftButton}>
+          { route.lefttButtonIcon
+            ? <Icon name={route.leftButtonIcon} size={24} />
+            : ' ' + route.leftButtonText
+          }
+        </LeftButton>
+      )
+    }
+
+    // First route or manually disabled
+    if (index === 0) {
+      return null
+    }
+
+    // Default: use previous
+    let previousRoute = navState.routeStack[index - 1]
+    return (
+      <LeftButton onPress={() => {navigator.popToRoute(previousRoute)}}>
+       { ' ' + previousRoute.title}
+      </LeftButton>
+    )
+  },
+
+  RightButton: function(route, navigator, index, navState) {
+    if (typeof route.rightElement !== 'undefined') {
+      return route.rightElement
+    }
+
+    if (route.rightButtonIcon || route.rightButtonText) {
+      return (
+        <RightButton onPress={route.onPressRightButton}>
+          { route.rightButtonIcon
+            ? <Icon name={route.rightButtonIcon} size={24} />
+            : route.rightButtonText
+          }
+        </RightButton>
+      )
+    }
+  },
+
+  Title: function(route, navigator, index, navState) {
+    return (
+      <Text style={[styles.navBarText, styles.navBarTitleText]}>
+        {route.title}
+      </Text>
+    )
+  },
+
+}
+
+/* Route options:
+ * component
+ * passProps
+ * title
+ * appearFrom ['bottom', 'left']
+ *** Left button options
+ * leftElement
+ * leftButtonText
+ * leftButtonIcon
+ * onPressLeftButton
+ *** Right button options
+ * rightElement
+ * rightButtonText
+ * rightButtonIcon
+ * onPressRightButton
+*/
+
+class Navigation extends Component {
+  renderScene(route, navigator) {
+    this.props.expandRoute && this.props.expandRoute(route, navigator)
+    return <route.component ref={(elem) => {route.ref = elem}} navigator={navigator} {...route.passProps} />
+  }
+
+  configureScene(route) {
+    switch (route.appearFrom) {
+    case 'left':
+      return Navigator.SceneConfigs.FloatFromLeft
+    case 'bottom':
+      return Navigator.SceneConfigs.FloatFromBottom
+    default:
+      return Navigator.SceneConfigs.PushFromRight
+    }
+  }
+
+  onDidFocus(route) {
+    // Allow the route to listen to check if it's active
+    route.ref.onActiveRoute && route.ref.onActiveRoute()
+  }
+
+  render() {
+    return (
+       <Navigator ref="nav"
+         {...this.props}
+         onDidFocus={this.onDidFocus.bind(this)}
+         renderScene={this.renderScene.bind(this)}
+         configureScene={this.configureScene.bind(this)}
+         sceneStyle={styles.scene}
+         navigationBar={this.props.hideNavBar ? null : <Navigator.NavigationBar style={styles.navBar}
+           routeMapper={NavigationBarRouteMapper}
+           />}
+       />
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  navBar: {
+    backgroundColor: '#F7F7F7',
+    borderBottomColor: '#CECED2',
+    borderBottomWidth: StyleSheet.hairlineWidth,
+  },
+  navBarText: {
+    fontSize: 16,
+    marginVertical: 10,
+  },
+  navBarTitleText: {
+    color: '#373E4D',
+    fontWeight: 'bold',
+    marginVertical: 9,
+  },
+  navBarLeftButton: {
+    paddingLeft: 10,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  navBarBackIcon: {
+    fontSize: 30,
+    marginTop: 2,
+  },
+  navBarRightButton: {
+    paddingRight: 10,
+  },
+  navBarButtonText: {
+    color: '#5890FF',
+  },
+  disabled: {
+    color: "black",
+    opacity: 0.6,
+  },
+  scene: {
+    flex: 1,
+    flexDirection: 'column',
+    paddingTop: 64,
+    backgroundColor: '#EFEFF4',
+  },
+})
+
+export default Navigation
+export {LeftButton, RightButton, styles}
